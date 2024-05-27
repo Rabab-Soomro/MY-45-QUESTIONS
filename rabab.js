4521cb21e47fa9ced4c491a4f4dfc4f187b586df
Rabab-Soomro <nalishba76@gmail.com>
2024-05-27 10:46:24 +0500

    Add files via upload

--- rabab.js
+++ rabab.js
@@ -1,3 +1,13 @@
-//Q 9:Favourite Number:Store your favourite number in a variable.Then, using that variable create a message tahat reveals your favourite number.print that message.
-var favouriteNumber = 1;
-console.log("My Favourite Number Is", favouriteNumber);
+//Q 11:Names:store names of your few friends in an array called names.print each person name by accessing each element in the list.one at a time.
+var friendName = ["Rabab", "Sumbul", "Tooba", "Ruman"];
+console.log(friendName[0]);
+console.log(friendName[1]);
+console.log(friendName[2]);
+console.log(friendName[3]);
+//Q 12
+//same as the question 11 but also print the message
+var friendName = ["Rabab", "Sumbul", "Tooba", "Ruman"];
+var message = "How are you??";
+console.log("hellow", friendName[0], message);
+console.log("hellow", friendName[1], message);
+console.log("hellow", friendName[2], message);
--- rabab.ts
+++ rabab.ts
@@ -1,4 +1,7 @@
-//Q 9:Favourite Number:Store your favourite number in a variable.Then, using that variable create a message tahat reveals your favourite number.print that message.
-let favouriteNumber=1;
-console.log("My Favourite Number Is",favouriteNumber);
-//Q 10:add comments: i havve already put comments in my typescrpt program;
+//Q 11:Names:store names of your few friends in an array called names.print each person name by accessing each element in the list.one at a time.
+let friendName=["Rabab","Sumbul","Tooba","Ruman"]
+console.log(friendName[0]);
+console.log(friendName[1]);
+console.log(friendName[2]);
+console.log(friendName[3]);
+
