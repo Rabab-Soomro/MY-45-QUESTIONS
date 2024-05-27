9cab65ef310272a730bc8dcb9d069c8c4f8445ee
Rabab-Soomro <nalishba76@gmail.com>
2024-05-27 10:53:31 +0500

    Add files via upload

--- rabab.js
+++ rabab.js
@@ -1,7 +1,7 @@
-//Q 12:same as Q11 but add message also
-var friendName = ["Rabab", "Sumbul", "Tooba", "Ruman"];
-var message = "How Are You???";
-console.log("hellow", friendName[0], message);
-console.log("hellow", friendName[1], message);
-console.log("hellow", friendName[2], message);
-console.log("hellow", friendName[3], message);
+/* Q 13:Your own array:Think of your favourite mode of transpotation,such as motorcycle or a car,and make list that several examples.
+use your llst to print a  series of statements about these items such as"I would like to own a Honda Motorcycle."*/
+var transpotationMode = ["TOYOTA COROLLA", "LAND CRUISER PRADO", "SUV"];
+transpotationMode.forEach(function (Mode) {
+    return console.log("I would  like to buy ".concat(Mode));
+});
+console.log("But ".concat(transpotationMode[1], " is my dream Car"));
--- rabab.ts
+++ rabab.ts
@@ -1,7 +1,8 @@
-//Q 12:same as Q11 but add message also
-let friendName=["Rabab","Sumbul","Tooba","Ruman"]
-let message="How Are You???"
-console.log("hellow",friendName[0],message);
-console.log("hellow",friendName[1],message);
-console.log("hellow",friendName[2],message);
-console.log("hellow",friendName[3],message);
+/* Q 13:Your own array:Think of your favourite mode of transpotation,such as motorcycle or a car,and make list that several examples.
+use your llst to print a  series of statements about these items such as"I would like to own a Honda Motorcycle."*/
+let transpotationMode=["TOYOTA COROLLA","LAND CRUISER PRADO","SUV"]
+ transpotationMode.forEach((Mode) =>      
+      console.log(`I would  like to buy ${Mode}`)  
+ );
+ console.log(`But ${transpotationMode[1]} is my dream Car`);
+    
