dd147c8c9bea7a6cc90dfc05f312b2601ec4c451
Rabab-Soomro <nalishba76@gmail.com>
2024-05-27 10:51:04 +0500

    Add files via upload

--- rabab.ts
+++ rabab.ts
@@ -1,7 +1,7 @@
-//Q 11:Names:store names of your few friends in an array called names.print each person name by accessing each element in the list.one at a time.
+//Q 12:same as Q11 but add message also
 let friendName=["Rabab","Sumbul","Tooba","Ruman"]
-console.log(friendName[0]);
-console.log(friendName[1]);
-console.log(friendName[2]);
-console.log(friendName[3]);
-
+let message="How Are You???"
+console.log("hellow",friendName[0],message);
+console.log("hellow",friendName[1],message);
+console.log("hellow",friendName[2],message);
+console.log("hellow",friendName[3],message);
