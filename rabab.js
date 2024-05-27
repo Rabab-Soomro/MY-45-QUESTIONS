05af312c61771e71f3de297869556c1cb8d78d37
Rabab-Soomro <nalishba76@gmail.com>
2024-05-27 10:39:17 +0500

    Add files via upload

---
+++ rabab.js
@@ -0,0 +1,5 @@
+//Q7:Number Eight:Write addition,subtraction,multiplication,and division operation that each result in the number eight.Be sure to enclose your opreation in print statement to see the resuult.
+console.log(4 + 4);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
